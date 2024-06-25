0cc6a3d95789d5dff02160ca90f2a7f7a42d43a1
Kuaskuas <166414311+Kuaskuas@users.noreply.github.com>
2024-06-25 10:35:07 -0300

    Delete TP5/TP5_2resuelto.sh

--- TP5/TP5_2resuelto.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-API_KEY="c1a435814aca4cf7b7f231956242206"
-echo "Ingrese el nombre de la ciudad que desea buscar (debe ingresarlo todo junto)"
-read  CITY
-
-URL="http://api.weatherapi.com/v1/current.json?key=$API_KEY&q=$CITY"
-
-curl -s "$URL" | json_pp
