58d45387b22e4ea6543744eb31905cfd2559553d
Kuaskuas <166414311+Kuaskuas@users.noreply.github.com>
2024-06-25 10:34:44 -0300

    Rename TP5_correccion.unknown to TP5_correccion.sh

--- TP5/TP5_correccion.sh
+++ TP5/TP5_correccion.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Configuración de la API
+# ConfiguraciÃ³n de la API
 API_CLAVE="c1a435814aca4cf7b7f231956242206"
 API_URL="http://api.weatherapi.com/v1/current.json"
 
-# Función para obtener el clima de una ciudad
+# FunciÃ³n para obtener el clima de una ciudad
 obtener_clima() {
   local ciudad=$1
   local respuesta=$(curl -s -X GET \
     "${API_URL}?key=${API_CLAVE}&q=${ciudad}")
   local clima=$(echo "$respuesta" | jq -r '.current.condition.text')
   local temperatura=$(echo "$respuesta" | jq -r '.current.temp_c')
-  echo "El clima en ${ciudad} es ${clima} con una temperatura de ${temperatura}°C."
+  echo "El clima en ${ciudad} es ${clima} con una temperatura de ${temperatura}Â°C."
 }
 
 # Preguntar al usuario por la ciudad
